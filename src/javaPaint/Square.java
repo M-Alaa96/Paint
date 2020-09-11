af441e6737441fedbe2969ddd03c45a6b980dd76
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 20:22:49 +0200

    Update Square.java

--- src/javaPaint/Square.java
+++ src/javaPaint/Square.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaPaint;
 
 import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
-/**
- *
- * @author Kdeep
- */
 public class Square extends Rectangle{
     
     
