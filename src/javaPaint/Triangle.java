ad09732fd233adea42cc3344f4378eecdfb58216
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 20:23:42 +0200

    Update Triangle.java

--- src/javaPaint/Triangle.java
+++ src/javaPaint/Triangle.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaPaint;
 
 import java.awt.Color;
 import java.awt.Polygon;
 
-/**
- *
- * @author Kdeep
- */
 public class Triangle extends Shape {
     
     private Point secondPoint, thirdPoint;
