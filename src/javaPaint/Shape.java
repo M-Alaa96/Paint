d6ccc7f612d2987bd941ca1a5936f76d126ff341
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 20:21:41 +0200

    Update Shape.java

--- src/javaPaint/Shape.java
+++ src/javaPaint/Shape.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaPaint;
 
 import java.awt.Color;
 
-/**
- *
- * @author Kdeep
- */
 public abstract class Shape implements Cloneable{
     
     protected Point startPoint;
