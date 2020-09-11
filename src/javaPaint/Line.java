35cb2c51bbeed3213a504a4265377641fd57af9a
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 20:15:20 +0200

    Update Line.java

--- src/javaPaint/Line.java
+++ src/javaPaint/Line.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaPaint;
 
 import java.awt.Color;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 
-/**
- *
- * @author Kdeep
- */
 public class Line extends Shape{
     private Point endPoint;
 
